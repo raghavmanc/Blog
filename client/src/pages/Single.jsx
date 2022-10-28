915902e877de4ce7921802621d3ee8331b42a7d3
raghav bhardwaj <raghavmanc@gmail.com.com>
2022-10-27 20:51:53 -0400

    Designed Single post page and added styles

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import Menu from '../components/Menu'
+import Delete from '../images/862px-Delete-button.svg.png'
+import Edit from '../images/UniversalEditButton3.svg.png'
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+    <div className="content">
+      <img src='https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=600' alt='name'></img>
+      
+      <div className="user">
+      <img src="https://images.pexels.com/photos/3772510/pexels-photo-3772510.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+      <div className="info">
+        <span>Raghav</span>
+        <p>Posted 7 days ago</p>
+      </div>
+      <div className="edit">
+        <Link to={`/write?edit=2`}><img src={Edit} alt="" /></Link>
+        <Link><img src={Delete} alt="" /></Link>
+      </div>
+      </div>
+      <h1>Title</h1>
+      <p>Paris is synonymous with the finesParis is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 DaysParis is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 DaysParis is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 DaysParis is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days</p>
+    </div>
+
+
+    <div className="menu">
+      <Menu/>
+    </div>
+    </div>
   )
 }
 
