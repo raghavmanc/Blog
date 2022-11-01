b55a50539bdbe91bcb909ed8819b77be44813223
raghav bhardwaj <raghavmanc@gmail.com.com>
2022-11-01 11:14:09 -0400

    Implemented auth.js (Login and Register functionality) using axios and completed connection with mySQL

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,8 @@
 import express from 'express'
-import { login, logout, register } from '../controllers/user.js';
 
 
 const router = express.Router();
 
-router.post('/register',register);
-router.post('/login',login);
-router.post('/logout',logout);
 
 
 export default router
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import {useState} from 'react'
+import axios from 'axios'
 
 const Register = () => {
+
+const [inputs,setInputs] = useState({
+  username: "",
+  email: "",
+  password: ""
+})
+
+const [err, setError] = useState(null)
+
+// Whenever the input changes we update the state value using handleChange
+
+const handleChange = e => {
+  //6 state mistakes react lamadev - checkout the video
+  setInputs( prev => ({ ...prev, [e.target.name] : e.target.value }))
+} 
+
+const navigate = useNavigate()
+
+//async function because we are making a request to an API - install axios in client 
+const handleSubmit = async e => {
+
+    e.preventDefault() // Because we dont want to refreseh our page when we hit the register button 
+    try{
+      await axios.post("/auth/register", inputs)
+      navigate("/login")
+    }catch(err){
+      setError(err.response.data);
+    } 
+} 
+
   return (
     <div className='auth'>
-      <h1>Register</h1>
+      <h1>Register</h1> 
 
       <form>
-         <input required type="text" placeholder='username'></input>
-         <input required type="email" placeholder='email'></input>
-         <input required type="password" placeholder='password'></input>
-        <button>Login</button>
-        <p>This is an error!</p>
+         <input required name = "username" type="text" placeholder='username' onChange={handleChange}></input>
+         <input required name = "email" type="email" placeholder='email'  onChange={handleChange}></input>
+         <input required name = "password" type="password" placeholder='password'  onChange={handleChange}></input>
+        <button onClick={handleSubmit}>Register</button>
+        { err && <p>{err}</p>}
         <span>Already have an account?
           <Link to="/login"> Login </Link>
         </span>
